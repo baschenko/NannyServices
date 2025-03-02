f5c732b3e1f03cd535d0fb17c5d644b43dfc2ce9
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2025-03-02 13:14:46 +0200

    update nav not home

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,11 +1,18 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation.jsx';
 import css from './AppBar.module.css';
 import UserAuth from '../UserAuth/UserAuth.jsx';
+import clsx from 'clsx';
 
 export default function AppBar() {
+  const location = useLocation();
+
+  const buildHeaderClass = () => {
+    return clsx(css.header, location.pathname !== '/' && css.isNotHome);
+  };
+
   return (
-    <header className={css.header}>
+    <header className={buildHeaderClass()}>
       <NavLink to="/" className={css.logo}>
         <div>Nanny.Services</div>
       </NavLink>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import css from './HomePage.module.css';
 import Icon from '../../components/Icon/Icon.jsx';
-import clsx from 'clsx';
 
 const HomePage = () => {
   const location = useLocation();
